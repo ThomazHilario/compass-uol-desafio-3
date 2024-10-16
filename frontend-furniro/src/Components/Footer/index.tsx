dd962f6689b22716b734cdf2e750eeb6d3135711
Thomaz Alves <thomazhilario5@gmail.com>
2024-10-16 15:49:30 -0300

    feat(Footer.tsx): Construção do componente Footer
    - Adicionar o mobile-first ao footer.
    - Adicionar variáveis para cor das fontes.

--- frontend-furniro/src/Components/Footer/index.tsx
+++ frontend-furniro/src/Components/Footer/index.tsx
@@ -1,43 +1,58 @@
+// React Router dom
+import { Link } from 'react-router-dom'
+
 // css import
 import './footer.css'
 
 export const Footer = () => {
     return(
         <footer>
-            <ul>
-                <li>
-                    <h2>Furniro</h2>
-                </li>
+            <section>
+                <ul>
+                    <li>
+                        <h2>Furniro</h2>
 
-                <li className='links__furniro'>
-                    <h3>Links</h3>
+                        <address>
+                            400 University Drive Suite 200 Coral Gables,<br/>
+                            FL 33134 USA
+                        </address>
+                    </li>
 
-                    <menu>
-                        <li>Home</li>
-                        <li>Shop</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </menu>
-                </li>
+                    <li className='links__furniro'>
+                        <h3>Links</h3>
 
-                <li className='help__li'>
-                    <h3>Help</h3>
+                        <menu>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/shop'>Shop</Link></li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </menu>
+                    </li>
 
-                    <ul>
-                        <li>Payment Options</li>
-                        <li>Returns</li>
-                        <li>Privacy Policies</li>
-                    </ul>
-                </li>
+                    <li className='help__li'>
+                        <h3>Help</h3>
 
-                <li className='newsletter'>
-                    <h3>Newsletter</h3>
-                </li>
-            </ul>
+                        <ul>
+                            <li>Payment Options</li>
+                            <li>Returns</li>
+                            <li>Privacy Policies</li>
+                        </ul>
+                    </li>
 
-            <hr />
+                    <li className='newsletter'>
+                        <h3>Newsletter</h3>
 
-            <p>2024 Furniro. All rights reverved</p>
+                        <form>
+                            <input type="text" placeholder='Enter Your Email Address'/>
+                            <button type='submit'>SUBSCRIBE</button>
+                        </form>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <p>2024 Furniro. All rights reverved</p>
+            </section>
         </footer>
     )
 }
